c60bcd401893f9c00be8fb752d4dd109b8005623
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-03-22 21:13:03 +0100

    Input component was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import CompanyLogo from "./CompanyLogo/CompanyLogo";
+import { Input } from "./components/Input/Input.component";
+import SearchIcon from "./assets/magnifying-glass-solid.svg";
 
 function App() {
   return (
     <div>
       <CompanyLogo />
       <h1>RentMe</h1>
+      <Input icon={SearchIcon} placeholder="find what you need"></Input>
+      <Input placeholder="city"></Input>
     </div>
   );
 }
