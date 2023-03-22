6d6ff2fc95de2292bda07053c0517012f4a22af4
Graffiss <graffis@gmail.com>
2023-03-22 22:16:07 +0100

    Fixed CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
 import GlobalStyle from "../theme/globalStyles";
 import "./App.css";
 import CompanyLogo from "./CompanyLogo/CompanyLogo";
 
 function App() {
   return (
-  <>
-   <GlobalStyle />
-    <div>
-      <CompanyLogo />
-      <h1>RentMe</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        <CompanyLogo />
+        <h1>RentMe</h1>
+      </div>
     </>
   );
-
 }
 
 export default App;
