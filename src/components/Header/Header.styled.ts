19eee3902d85f76123dedbc10ae047db33df64e3
Dawid Maslanka <dawid.maslan@gmail.com>
2023-03-23 20:26:19 +0100

    JFDZR9ASU-27 created header

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const CompanyHeaderSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+`;
+export const CompanyHeaderLogo = styled.div`
+  flex: 1;
+`;
+
+export const CompanyHeaderSearch = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 2;
+`;
+
+export const CompanyHeaderSearchLeft = styled.div`
+  margin-right: 16px;
+`;
+export const CompanyHeaderSearchRight = styled.div`
+  margin-left: 16px;
+`;
+
+export const CompanyHeaderSign = styled.div`
+  flex: 1;
+  text-align: right;
+`;
