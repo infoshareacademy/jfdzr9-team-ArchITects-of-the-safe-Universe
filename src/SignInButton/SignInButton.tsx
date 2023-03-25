9b7a37f944c997283d57d4d648f10b278dd9644b
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-03-25 11:14:22 +0100

    Routing was prepared

--- src/SignInButton/SignInButton.tsx
+++ src/SignInButton/SignInButton.tsx
@@ -5,13 +5,10 @@ interface SignInButtonProps {
   children?: string;
 }
 
-// kolory buttonów będą podmienione po podłączeniu styli globalnych
-
 export const SignInButton = ({ children }: SignInButtonProps) => {
-  const [color, setColor] = useState("grey");
+  const [color, setColor] = useState("var(--color-light-grey)");
   const handleClick = () => {
-    setColor(color === "grey" ? "orange" : "grey");
-    // setColor(color === "var(--color-light-grey)" ? "var(--color-orange)" : "var(--color-light-grey)");
+    setColor(color === "var(--color-light-grey)" ? "var(--color-orange)" : "var(--color-light-grey)");
   };
   return (
     <ButtonS style={{ background: color }} onClick={handleClick}>
