d91d569caffc2481cd626a91f84f84e405ffeee3
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-03-25 12:48:48 +0100

    BrowserRouter was moved to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import GlobalStyle from "../theme/globalStyles";
 // import { Footer } from "./Footer/Footer.component.styled";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 // import { MainPage } from "./pages/MainPage";
 
@@ -8,22 +8,20 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* <Route path="/" element={<MainPage />} /> */}
-          {/* <Route path="/:howDoestItWork" element={<HowDoesItWork />} /> */}
-          {/* <Route path="/:howDoestItWork/borrow" element={<Borrow />} /> */}
-          {/* <Route path="/:howDoestItWork/lend" element={<Lend />} /> */}
-          {/* <Route path="account" element={<SignInPage />} /> */}
-          {/* <Route path="account/register" element={<RegisterPage />} /> */}
-          {/* <Route path="productPage" element={<ProductPage />} /> */}
-          {/* <Route path="addProductPage" element={<AddProductPage />} /> */}
-          {/* <Route path="opinions" element={<OpinionsPage />} /> */}
-          {/* <Route path="*" element={<Navigate to="not-found" replace={true} />} /> */}
-        </Routes>
-        {/* </Footer> */}
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<MainPage />} /> */}
+        {/* <Route path="/:howDoestItWork" element={<HowDoesItWork />} /> */}
+        {/* <Route path="/:howDoestItWork/borrow" element={<Borrow />} /> */}
+        {/* <Route path="/:howDoestItWork/lend" element={<Lend />} /> */}
+        {/* <Route path="account" element={<SignInPage />} /> */}
+        {/* <Route path="account/register" element={<RegisterPage />} /> */}
+        {/* <Route path="productPage" element={<ProductPage />} /> */}
+        {/* <Route path="addProductPage" element={<AddProductPage />} /> */}
+        {/* <Route path="opinions" element={<OpinionsPage />} /> */}
+        {/* <Route path="*" element={<Navigate to="not-found" replace={true} />} /> */}
+      </Routes>
+      {/* </Footer> */}
     </>
   );
 }
