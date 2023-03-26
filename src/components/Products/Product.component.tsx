ef6634ef7c9298852e41a0bbce75d4836b001177
PIotr Stefanski <peterstefanski3@gmail.com>
2023-03-26 13:31:56 +0200

    Add slider(doesn't work) without npm i slider

--- src/components/Products/Product.component.tsx
+++ src/components/Products/Product.component.tsx
@@ -1,5 +1,6 @@
 import { Navigator, ArrowLeft, ProductContainer, ArrowRight } from "./Product.styled";
 import { Container, ContainerPhoto, ContainerText } from "./Product.styled";
+// import ReactCardSlider from 'react-card-slider-component';
 
 export const Products = () => {
   return (
@@ -10,11 +11,13 @@ export const Products = () => {
         </div>
       </ArrowLeft>
       <ProductContainer>
+        {/* const slides = [ */}
         <Container>
           <ContainerPhoto />
           <ContainerText>
             <h3>Name</h3>
             <p>Book 1</p>
+            {/* {image:"https://picsum.photos/200/300",title:"This is a title",description:"This is a description",clickEvent:sliderClick}, */}
           </ContainerText>
         </Container>
         <Container>
@@ -22,6 +25,7 @@ export const Products = () => {
           <ContainerText>
             <h3>Name</h3>
             <p>Book 2</p>
+            {/* {image:"https://picsum.photos/600/500",title:"This is a second title",description:"This is a second description",clickEvent:sliderClick}, */}
           </ContainerText>
         </Container>
         <Container>
@@ -29,6 +33,7 @@ export const Products = () => {
           <ContainerText>
             <h3>Name</h3>
             <p>Book 3</p>
+            {/* {image:"https://picsum.photos/700/600",title:"This is a third title",description:"This is a third description",clickEvent:sliderClick}, */}
           </ContainerText>
         </Container>
         <Container>
@@ -36,6 +41,7 @@ export const Products = () => {
           <ContainerText>
             <h3>Name</h3>
             <p>Book 4</p>
+            {/* {image:"https://picsum.photos/500/400",title:"This is a fourth title",description:"This is a fourth description",clickEvent:sliderClick}, */}
           </ContainerText>
         </Container>
         <Container>
@@ -43,8 +49,10 @@ export const Products = () => {
           <ContainerText>
             <h3>Name</h3>
             <p>Book 1</p>
+            {/* {image:"https://picsum.photos/200/300",title:"This is a fifth title",description:"This is a fifth description",clickEvent:sliderClick}, */}
           </ContainerText>
         </Container>
+        {/* ] */}
       </ProductContainer>
       <ArrowRight>
         <div>
@@ -53,4 +61,5 @@ export const Products = () => {
       </ArrowRight>
     </Navigator>
   );
+  // <ReactCardSlider slides={slides}/>
 };
