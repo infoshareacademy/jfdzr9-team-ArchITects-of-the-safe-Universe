5871c64793f3f2e05d248c32ceb3b8dd989dfe13
Dawid Maslanka <dawid.maslan@gmail.com>
2023-04-19 21:58:24 +0200

    fixed

--- src/UserPanelButton/UserPanelButton.tsx
+++ src/UserPanelButton/UserPanelButton.tsx
@@ -1,11 +1,12 @@
 import { ButtonS } from "../components/Buttons/Button.styled";
 import { Link } from "react-router-dom";
+import React from "react";
 
 interface UserPanelButtonProps {
   onClick: () => void;
 }
 
-export const UserPanelButton = ({ onClick }: UserPanelButtonProps) => {
+export const UserPanelButton: React.FC<UserPanelButtonProps> = ({ onClick }) => {
   return (
     <Link to="/userPanelBorrow">
       <ButtonS onClick={onClick}>Panel użytkownika</ButtonS>
