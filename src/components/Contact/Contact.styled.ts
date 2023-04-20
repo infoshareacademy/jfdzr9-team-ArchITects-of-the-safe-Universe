e4090c5e2e8de603978b506b1d718fad1e0b92ad
Jolanta Marszałek <jolantamarszalek3@gmail.com>
2023-04-20 13:30:50 +0200

    Changed styled in Contact

--- src/components/Contact/Contact.styled.ts
+++ src/components/Contact/Contact.styled.ts
@@ -6,35 +6,6 @@ export const FormContainer = styled.form`
   align-items: center;
 `;
 
-export const FormGroup = styled.div`
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 600px;
-`;
-
-export const FormGroupImg = styled.div`
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 600px;
-`;
-
-export const FormGroupNextTo = styled.div`
-  margin-top: 20px;
-  display: flex;
-  justify-content: center;
-  width: 600px;
-  gap: 40px;
-`;
-
-export const Label = styled.label`
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
-
 export const Input = styled.input`
   padding: 10px;
   border: 1px solid #ccc;
@@ -49,15 +20,6 @@ export const Input = styled.input`
   }
 `;
 
-export const InputFile = styled.input`
-  padding: 10px;
-  border: none;
-  font-size: 16px;
-  margin-bottom: 10px;
-  width: 300px;
-  height: 50px;
-`;
-
 export const TextArea = styled.textarea`
   padding: 50px;
   border: 1px solid #ccc;
@@ -68,19 +30,5 @@ export const TextArea = styled.textarea`
   @media (max-width: 40em) {
     height: 25px;
     font-size: 14px;
-  }
-`;
-
-export const Select = styled.select`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  margin-bottom: 10px;
-  width: 222px;
-
-  @media (max-width: 40em) {
-    height: 30px;
-    padding: 0px;
   }
 `;
