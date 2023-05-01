aa49df12be2777dd76dea7bfd1dae3ce43639b57
PIotr Stefanski <peterstefanski3@gmail.com>
2023-05-01 22:17:13 +0200

    add footer's styles

--- src/Footer/Footer.component.tsx
+++ src/Footer/Footer.component.tsx
@@ -1,32 +1,63 @@
 import { Link } from "react-router-dom";
 import { Footer } from "../Footer/Footer.styled";
 import GitHubLogo from "../assets/iconmonstr-github.svg";
+import {
+  LogoGit,
+  Rightside,
+  Leftside,
+  Law,
+  AppTeam,
+  Authors,
+  Dawid,
+  Jolanta,
+  Katarzyna,
+  Piotr,
+} from "../Footer/Footer.styled";
 
 export const FooterLabel = () => {
   return (
     <Footer>
-      <div>
-        <h3>
-          RentMe, @2023 rentme Inc.
-          <br />
-          Wszystkie prawa zastrzeżone.
-        </h3>
-        <Link to="https://github.com/infoshareacademy/jfdzr9-team-ArchITects-of-the-safe-Universe">
-          <img src={GitHubLogo} alt="github logo" />
-        </Link>
-      </div>
-      <div>
-        <h3 style={{ textDecoration: "underline" }}>Twórcy aplikacji:</h3>
-        <h3>
-          Dawid Maślanka
-          <br />
-          Jolanta Marszałek
-          <br />
-          Katarzyna Fronc
-          <br />
-          Piotr Stefański
-        </h3>
-      </div>
+      <Leftside>
+        <Law>
+          <div>
+            <h3>
+              RentMe, @2023 rentme Inc.
+              <br />
+              Wszystkie prawa zastrzeżone.
+            </h3>
+          </div>
+        </Law>
+        /
+        <div>
+          <Link to="https://github.com/infoshareacademy/jfdzr9-team-ArchITects-of-the-safe-Universe">
+            <img src={GitHubLogo} alt="github logo" />
+          </Link>
+        </div>
+        {/* </LogoGit> */}
+      </Leftside>
+      <Rightside>
+        <AppTeam>
+          <div>
+            <h3 style={{ textDecoration: "underline" }}>Twórcy aplikacji:</h3>
+          </div>
+        </AppTeam>
+        <Authors>
+          <div>
+            <Dawid>
+              <h3>Dawid Maślanka</h3>
+            </Dawid>
+            <Jolanta>
+              <h3>Jolanta Marszałek</h3>
+            </Jolanta>
+            <Katarzyna>
+              <h3>Katarzyna Fronc</h3>
+            </Katarzyna>
+            <Piotr>
+              <h3>Piotr Stefański</h3>
+            </Piotr>
+          </div>
+        </Authors>
+      </Rightside>
     </Footer>
   );
 };
--- src/Footer/Footer.styled.ts
+++ src/Footer/Footer.styled.ts
@@ -7,7 +7,7 @@ export const Footer = styled.footer`
   justify-content: space-evenly;
   margin: auto;
   padding: 5px;
-  height: 60px;
+  height: 80px;
   width: 100%;
   background-color: var(--color-very-light-grey);
   font-size: 0.5rem;
@@ -22,4 +22,76 @@ export const Footer = styled.footer`
     padding: 0px;
     padding-top: 5px;
   }
+`;
+
+export const Law = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  background-color: var(--color-very-light-grey);
+  font-size: 0.8rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+  padding-right: 10px;
+`;
+
+export const AppTeam = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const Authors = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const Leftside = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: space-around;
+`;
+
+export const Rightside = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const LogoGit = styled.div`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Dawid = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const Jolanta = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const Katarzyna = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
+`;
+
+export const Piotr = styled.div`
+  background-color: var(--color-very-light-grey);
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: var(--color-dark-grey);
 `;
