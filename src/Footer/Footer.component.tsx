e285230c510141d05a93e972c71b3cc2c94b1b0c
PIotr Stefanski <peterstefanski3@gmail.com>
2023-05-03 21:32:00 +0200

    footer chenges with links pictures

--- src/Footer/Footer.component.tsx
+++ src/Footer/Footer.component.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import { Footer } from "../Footer/Footer.styled";
 import GitHubLogo from "../assets/iconmonstr-github.svg";
+import GitHubLogo2 from "../assets/iconmonstr-github copy.svg";
+import Linkedin from "../assets/iconmonstr-linkedin-3.svg";
 import {
-  LogoGit,
   Rightside,
   Leftside,
   Law,
   AppTeam,
   Authors,
+  Space,
+  SpaceMini,
+  RightName,
+  LeftName,
   Dawid,
   Jolanta,
   Katarzyna,
@@ -27,13 +32,9 @@ export const FooterLabel = () => {
             </h3>
           </div>
         </Law>
-        /
-        <div>
-          <Link to="https://github.com/infoshareacademy/jfdzr9-team-ArchITects-of-the-safe-Universe">
-            <img src={GitHubLogo} alt="github logo" />
-          </Link>
-        </div>
-        {/* </LogoGit> */}
+        <Link to="https://github.com/infoshareacademy/jfdzr9-team-ArchITects-of-the-safe-Universe">
+          <img src={GitHubLogo} alt="github logo" />
+        </Link>
       </Leftside>
       <Rightside>
         <AppTeam>
@@ -42,20 +43,57 @@ export const FooterLabel = () => {
           </div>
         </AppTeam>
         <Authors>
-          <div>
+          <RightName>
+            {" "}
             <Dawid>
               <h3>Dawid Maślanka</h3>
+              <SpaceMini />
+              <Link to="https://github.com/DawidMaslanka">
+                <img src={GitHubLogo2} alt="github logo2" />
+              </Link>
+              <SpaceMini />
+              <Link to="https://www.linkedin.com/in/dawid-maslanka/">
+                <img src={Linkedin} alt="Linkedin" />
+              </Link>
             </Dawid>
             <Jolanta>
               <h3>Jolanta Marszałek</h3>
+              <SpaceMini />
+              <Link to="https://github.com/JolantaMarszalek">
+                <img src={GitHubLogo2} alt="github logo2" />
+              </Link>
+              <SpaceMini />
+              <Link to="https://www.linkedin.com/in/jolanta-marsza%C5%82ek/">
+                <img src={Linkedin} alt="Linkedin" />
+              </Link>
             </Jolanta>
+          </RightName>
+          <Space />
+          <LeftName>
+            {" "}
             <Katarzyna>
               <h3>Katarzyna Fronc</h3>
+              <SpaceMini />
+              <Link to="https://github.com/katarzynaFronc">
+                <img src={GitHubLogo2} alt="github logo2" />
+              </Link>
+              <SpaceMini />
+              <Link to="https://www.linkedin.com/in/katarzyna-f/">
+                <img src={Linkedin} alt="Linkedin" />
+              </Link>
             </Katarzyna>
             <Piotr>
               <h3>Piotr Stefański</h3>
+              <SpaceMini />
+              <Link to="https://github.com/peter-stefanski">
+                <img src={GitHubLogo2} alt="github logo2" />
+              </Link>
+              <SpaceMini />
+              <Link to="https://www.linkedin.com/in/stefanski-piotr/">
+                <img src={Linkedin} alt="Linkedin" />
+              </Link>
             </Piotr>
-          </div>
+          </LeftName>
         </Authors>
       </Rightside>
     </Footer>
--- src/Footer/Footer.styled.ts
+++ src/Footer/Footer.styled.ts
@@ -4,13 +4,17 @@ export const Footer = styled.footer`
   position: fixed;
   bottom: 0;
   display: flex;
-  justify-content: space-evenly;
+  left: 0;
+  bottom: 0;
+  /* justify-content: space-evenly; */
   margin: auto;
   padding: 5px;
   height: 80px;
   width: 100%;
+  min-height: 8vh;
+  max-height: 8vh;
   background-color: var(--color-very-light-grey);
-  font-size: 0.5rem;
+  /* font-size: 16px; */
   font-weight: 200;
   color: var(--color-dark-grey);
   border-top-style: solid;
@@ -31,67 +35,76 @@ export const Law = styled.div`
   font-size: 0.8rem;
   font-weight: 200;
   color: var(--color-dark-grey);
-  padding-right: 10px;
+  min-height: auto;
+  max-height: auto;
+  /* padding-right: 100px; */
 `;
 
-export const AppTeam = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
-`;
-
-export const Authors = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
-`;
+export const AppTeam = styled.div``;
 
 export const Leftside = styled.div`
   display: flex;
+  position: sticky;
+  min-height: 8vh;
+  max-height: 8vh;
   flex-direction: row;
   flex-wrap: wrap;
   align-content: center;
   justify-content: space-around;
+  padding-right: 1200px;
 `;
 
 export const Rightside = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
+  /* font-size: 0.7rem; */
   font-weight: 200;
   color: var(--color-dark-grey);
+  font-size: 0.8rem;
 `;
 
-export const LogoGit = styled.div`
-  width: 200px;
-  height: 200px;
+export const Authors = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
+
+export const Space = styled.div`
+  width: 400px;
+`;
+
+export const SpaceMini = styled.div`
+  width: 20px;
+`;
+
+export const LeftName = styled.div``;
+
+export const RightName = styled.div``;
 
 export const Dawid = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 8px;
 `;
 
 export const Jolanta = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 8px;
 `;
 
 export const Katarzyna = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 8px;
 `;
 
 export const Piotr = styled.div`
-  background-color: var(--color-very-light-grey);
-  font-size: 0.7rem;
-  font-weight: 200;
-  color: var(--color-dark-grey);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 8px;
 `;
