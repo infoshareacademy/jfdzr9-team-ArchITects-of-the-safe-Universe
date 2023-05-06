6662ecae74eaa1184c6beab760d1a130d9e8918d
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-05-06 14:07:07 +0200

    Display how does it work page was fixed

--- src/pages/HowDoesItWorkLendPage.tsx
+++ src/pages/HowDoesItWorkLendPage.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import { ButtonM, ButtonContainer, OrangeButton } from "../components/Buttons/Button.styled";
 import { HowDoesItWorkLend } from "../components/HowDoesItWork/HowDoesItWork.component";
-import { ButtonStartRenting } from "../components/HowDoesItWork/HowDoesItWork.styled";
+import { ButtonStartRenting, HowWork } from "../components/HowDoesItWork/HowDoesItWork.styled";
 import { Title } from "../UI/Title.styled";
 import { StartRentingButton } from "../StartRentingButton/StartRentingButton";
 
 export const HowDoesItWorkLendPage = () => {
   return (
-    <>
+    <HowWork>
       <Title>Jak to działa?</Title>
       <ButtonContainer>
         <Link to="/howDoesItWork/borrow">
@@ -19,6 +19,6 @@ export const HowDoesItWorkLendPage = () => {
       <ButtonStartRenting>
         <StartRentingButton />
       </ButtonStartRenting>
-    </>
+    </HowWork>
   );
 };
